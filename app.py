c1d9144dc20ab6edf772674ea6399e63aa297e04
James Tessmer <james.tessmer@gmail.com>
2026-02-25 21:47:18 -0800

    Add super basic password gen that combines answers with a joining character

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template, request, session
 from dotenv import load_dotenv
 import random
 import os
-
+#
+# To do, on startup show a 'form' that prompts the user for the number of questions, and level of complexity they want in the password
+# Add a button that lets users go back to that form
+#
 app = Flask(__name__)
 
 load_dotenv()
@@ -11,14 +14,22 @@ all_questions = [
     {"label": "What is your name?", "name": "name"},
     {"label": "What is your favorite color?", "name": "color"},
     {"label": "What is your favorite food?", "name": "food"},
-    {"label": "What is your hobby?", "name": "hobby"},
+    {"label": "What is the street you live on?", "name": "street"},
     {"label": "Where are you from?", "name": "location"},
 ]
+join_chars = ['-','_','+','&','$','#','@','!','%','^','*','=']
+
     
     #randomly select x questions to show. We can probably prompt the user for
     # a number of words they want to have in the passphrase and select that many questions
 def generate_questions():
     return random.sample(all_questions, 3)
+
+def generate_password(answers: dict):
+    password = ""
+    for val in answers.values():
+        password = password + random.choice(join_chars) + val
+    return password
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -39,9 +50,10 @@ def index():
         # If Submit button was pressed we process answers in the text boxes
         if request.form.get("action") == "submit":
             answers = {q["name"]: request.form.get(q["name"]) for q in questions}
+            password = generate_password(answers)
             return render_template("index.html",
                                    questions=questions,
-                                   answers=answers)
+                                   password=password)
 
     return render_template("index.html",
                            questions=questions,
